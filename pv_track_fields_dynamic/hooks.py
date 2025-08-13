51c8faac834cd5f8f49efe4ab8af17bacf8a1b85
Vladi Parsonen <parsonenvladi@gmail.com>
2025-08-13 08:54:22 +0300

    Delete pv_track_fields_dynamic directory

--- pv_track_fields_dynamic/hooks.py
+++
@@ -1,3 +0,0 @@
-def uninstall_hook(cr, registry):
-    # Best-effort cleanup (not strictly necessary because of ondelete='cascade' on ir.model)
-    cr.execute("DELETE FROM track_fields_config")
--- pv_track_fields_dynamic/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import track_config
-from . import patcher  # installs the global create/write hooks
